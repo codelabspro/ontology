a2210f80084d7b86bd7da380b6adc19c6cce324e
arunabhdas <arunabhdas@gmail.com>
2023-09-01 03:22:19 -0400

    Created model_pipeline function

--- OntologyML/model_starter.py
+++ OntologyML/model_starter.py
@@ -1,5 +1,5 @@
 from transformers import ViltProcessor, ViltForQuestionAnswering
-import requests
+# import requests
 from PIL import Image
 
 # 470MB
@@ -7,15 +7,17 @@ processor = ViltProcessor.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
 model = ViltForQuestionAnswering.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
 
 # prepare image + question
-url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-image = Image.open(requests.get(url, stream=True).raw)
-text = "How many cats are there?"
+# url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+# image = Image.open(requests.get(url, stream=True).raw)
+# text = "How many cats are there?"
 
-# prepare inputs
-encoding = processor(image, text, return_tensors="pt")
+def model_pipeline(text: string, image: Image):
 
-# forward pass
-outputs = model(**encoding)
-logits = outputs.logits
-idx = logits.argmax(-1).item()
-print("Predicted answer:", model.config.id2label[idx])
+    # prepare inputs
+    encoding = processor(image, text, return_tensors="pt")
+
+    # forward pass
+    outputs = model(**encoding)
+    logits = outputs.logits
+    idx = logits.argmax(-1).item()
+    return model.config.id2label[idx]
